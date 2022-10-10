939a5987189dbdc84821f0ca3b552aa515ac99e6
beakeyz <joost.van.haag@gmail.com>
2022-10-10 07:31:35 +0000

    Test results: when disabling all interrupts, exept for keyboard (IRQ: 1) we clearly see the kernel dies when trying to serve an interrupt =/

--- src/arch/x86/interupts/control/pic.c
+++ src/arch/x86/interupts/control/pic.c
@@ -6,71 +6,73 @@
 
 uint16_t _irq_mask_cache = 0xffff;
 
-static void enable_vector (uint8_t vec) {
-    disable_interupts();
+static void enable_vector(uint8_t vec) {
+  disable_interupts();
 
-    if (!(_irq_mask_cache & (1 << vec))) {
-        return;
-    }
-    _irq_mask_cache &= ~(1 << vec);
+  if (!(_irq_mask_cache & (1 << vec))) {
+    return;
+  }
+  _irq_mask_cache &= ~(1 << vec);
 
-    uint8_t imr;
-    if (vec & 8) {
-        imr = in8(PIC2_DATA);
-        out8(PIC2_DATA, imr);
-    } else {
-        imr = in8(PIC1_DATA);
-        out8(PIC1_DATA, imr);
-    }
+  uint8_t imr;
+  if (vec & 8) {
+    imr = in8(PIC2_DATA);
+    out8(PIC2_DATA, imr);
+  } else {
+    imr = in8(PIC1_DATA);
+    out8(PIC1_DATA, imr);
+  }
 
-    enable_interupts();
+  enable_interupts();
 }
 
 void init_pic() {
 
-    uint8_t mask1 = in8(PIC1_DATA);
-    uint8_t mask2 = in8(PIC2_DATA);
+  uint8_t mask1 = in8(PIC1_DATA);
+  uint8_t mask2 = in8(PIC2_DATA);
 
-    // cascade init =D
-    out8(PIC1_COMMAND, ICW1_INIT|ICW1_ICW4); 
-    PIC_WAIT();
-    out8(PIC2_COMMAND, ICW1_INIT|ICW1_ICW4);
-    PIC_WAIT();
+  // cascade init =D
+  out8(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+  PIC_WAIT();
+  out8(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+  PIC_WAIT();
 
-    out8(PIC1_DATA, 0x20);
-    PIC_WAIT();
-	out8(PIC2_DATA, 0x28);
-    PIC_WAIT();
+  out8(PIC1_DATA, 0x20);
+  PIC_WAIT();
+  out8(PIC2_DATA, 0x28);
+  PIC_WAIT();
 
-	out8(PIC1_DATA, 0x04);
-    PIC_WAIT();
-	out8(PIC2_DATA, 0x02);
-    PIC_WAIT();
+  out8(PIC1_DATA, 0x04);
+  PIC_WAIT();
+  out8(PIC2_DATA, 0x02);
+  PIC_WAIT();
 
-	out8(PIC1_DATA, 1);
-    PIC_WAIT();
-    out8(PIC2_DATA, 1);
-    PIC_WAIT();
+  out8(PIC1_DATA, 1);
+  PIC_WAIT();
+  out8(PIC2_DATA, 1);
+  PIC_WAIT();
 
-    out8(PIC1_DATA, mask1);
-    out8(PIC2_DATA, mask2);
+  out8(PIC1_DATA, 0b11111101);
+  out8(PIC2_DATA, 0xFF);
+  //out8(PIC1_DATA, mask1);
+  //out8(PIC2_DATA, mask2);
 
-    //disable_pic();
+  // disable_pic();
 }
 
 void disable_pic() {
 
-    // send funnie
-    out8(PIC1_DATA, 0xFF);
-    out8(PIC2_DATA, 0xFF);
+  // send funnie
+  out8(PIC1_DATA, 0xFF);
+  out8(PIC2_DATA, 0xFF);
 
-    enable_vector(2);
+  enable_vector(2);
 }
 
 void pic_eoi(uint8_t num) {
-    // irq from pic2 :clown:, so send for both
-    if (num >= 8)
-        out8(PIC2_COMMAND, PIC_EOI_CODE);
+  // irq from pic2 :clown:, so send for both
+  if (num >= 8)
+    out8(PIC2_COMMAND, PIC_EOI_CODE);
 
-    out8(PIC1_COMMAND, PIC_EOI_CODE);
+  out8(PIC1_COMMAND, PIC_EOI_CODE);
 }
