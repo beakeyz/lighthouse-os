a146623ff04e6881f6882374864deb4bcdda56ea
beakeyz <joost.van.haag@gmail.com>
2023-02-06 01:20:48 +0000

    (1) Done some light testing. (2) added scaffolding for a new ipc/itc system called thread-interfaces

---
+++ src/aniva/proc/ipc/thr_intrf.c
@@ -0,0 +1,5 @@
+//
+// Created by joost on 2/6/23.
+//
+
+#include "thr_intrf.h"
---
+++ src/aniva/proc/ipc/thr_intrf.h
@@ -0,0 +1,37 @@
+#ifndef __ANIVA_THR_INTRF__
+#define __ANIVA_THR_INTRF__
+#include <libk/stddef.h>
+#include <libk/error.h>
+#include "proc/thread.h"
+
+/*
+ * TODO: let's actually figure out how we want to approach this problem...
+ */
+
+// some handy structs
+struct thread;
+struct threaded_socket;
+struct tspckt;
+
+typedef struct thread_interface {
+} thread_interface_t;
+
+/*
+ * When the current thread is compatible with the ANIVA thread interface model (i.e. sockets, drivers, etc.)
+ * we give it access to the interface that is mapped to this port
+ */
+ErrorOrPtr request_thread_if(uint32_t port);
+
+ErrorOrPtr attach_interface_to_thread(thread_t* thread);
+ErrorOrPtr detach_interface_from_thread(thread_t* thread);
+
+/*
+ * "Map" a given interface to a port
+ * TODO: lookup table for this or something else?
+ */
+ErrorOrPtr map_interface(uint32_t port);
+ErrorOrPtr unmap_interface(uint32_t port);
+
+ErrorOrPtr wait_for_if_signal();
+
+#endif //__ANIVA_THR_INTRF__
