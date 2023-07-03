f3c92c591d1448a483979a57bafabf187f2f84e3
Beakeyz <joost.van.haag@gmail.com>
2023-07-03 00:50:36 +0000

    Expanded the sys_exit function a little more
    this covers a few more cases, but the finer details should be worked out
    with more thought some other time

--- src/aniva/system/syscall/sys_exit/sys_exit.c
+++ src/aniva/system/syscall/sys_exit/sys_exit.c
@@ -5,6 +5,7 @@
 #include "proc/proc.h"
 #include "proc/thread.h"
 #include "sched/scheduler.h"
+#include "sync/atomic_ptr.h"
 
 /*
  * FIXME: what if a sub-thread of a certain process calls exit() ?
@@ -22,6 +23,21 @@ uintptr_t sys_exit_handler(uintptr_t code) {
   ASSERT_MSG(current_thread, "No current thread in the process");
   ASSERT_MSG(current_thread->m_parent_proc == current_proc, "Process to Thread mismatch!");
 
+  /* In this case we may always kill the entire process */
+  if (current_thread == current_proc->m_init_thread) {
+    goto exit_and_terminate;
+  } 
+
+  /* There are more threads in this process */
+  if (atomic_ptr_load(current_proc->m_thread_count) > 1) {
+    /* Identify if this thread */
+    bool should_thread_die = (!thread_is_socket(current_thread)); 
+
+    thread_set_state(current_thread, should_thread_die ? DYING : BLOCKED);
+
+    goto exit_and_yield;
+  }
+
   /*
    * This process was a shared library or a socket that finished initilization and we are waiting on 
    * the signal for destruction
@@ -32,12 +48,17 @@ uintptr_t sys_exit_handler(uintptr_t code) {
 
     /* This thread will be idle forever */
     thread_set_state(current_thread, BLOCKED);
-  } else 
-    try_terminate_process(current_proc);
+
+    goto exit_and_yield;
+  }
+
+exit_and_terminate:
+  try_terminate_process(current_proc);
 
   print("(debug) Process terminated with code: ");
   println(to_string(code));
 
+exit_and_yield:
   scheduler_yield();
 
   kernel_panic("Somehow returned to an exited process?");
