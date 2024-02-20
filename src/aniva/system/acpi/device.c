125a779b7365f650cb2b3b7e78beec12b76b7402
beakeyz <joost.van.haag@gmail.com>
2024-02-20 01:22:26 +0000

    Small lil fix

--- src/aniva/system/acpi/device.c
+++ src/aniva/system/acpi/device.c
@@ -1,39 +1,77 @@
 #include "acpi.h"
 #include "acpica/acpi.h"
+#include "libk/data/linkedlist.h"
 #include "libk/flow/error.h"
+#include <mem/heap.h>
 #include "system/acpi/acpica/acexcep.h"
+#include "system/acpi/acpica/aclocal.h"
 #include "system/acpi/acpica/acnamesp.h"
 #include "system/acpi/acpica/acpixf.h"
 #include "system/acpi/acpica/actypes.h"
+#include "system/acpi/device.h"
 #include "system/acpi/tables.h"
+
+static list_t* _acpi_device_list;
+
+static kerror_t acpi_add_device(acpi_handle_t handle, int type)
+{
+  acpi_device_t* device;
+  ACPI_DEVICE_INFO* info = nullptr;
+
+  device = kmalloc(sizeof(*device));
+  if (!device)
+    return -KERR_NOMEM;
+
+  AcpiGetObjectInfo(handle, &info);
+  if (!info) {
+    printf("Failed to get object info\n");
+    return AE_OK;
+  }
+
+  printf (" HID: %s, ADR: %llX, Name: %x\n", info->HardwareId.String, info->Address, info->Name);
+
+  list_append(_acpi_device_list, device);
+  return KERR_NONE;
+}
 
 ACPI_STATUS register_acpi_device(acpi_handle_t dev, uint32_t lvl, void* ctx, void** ret)
 {
+  kerror_t error;
   ACPI_STATUS Status;
-  ACPI_DEVICE_INFO* Info;
+  ACPI_OBJECT_TYPE obj_type;
   ACPI_BUFFER Path;
+  acpi_handle_t tmp;
   char Buffer[256];
 
   Path.Length = sizeof (Buffer);
   Path.Pointer = Buffer;
 
+  if (ACPI_FAILURE(AcpiGetType(dev, &obj_type)))
+    return AE_OK;
+
+  if (ACPI_FAILURE(AcpiGetHandle(dev, "_HID", &tmp)))
+    return AE_OK;
+
   /* Get the full path of this device and print it */
   Status = AcpiNsHandleToPathname (dev, &Path, true);
 
   if (ACPI_SUCCESS (Status))
-  {
     printf ("%s: ", (char*)Path.Pointer);
+
+  /* TODO: what to do on different types? */
+  switch (obj_type) {
+    case ACPI_TYPE_DEVICE:
+    case ACPI_TYPE_ANY:
+      error = acpi_add_device(dev, obj_type);
+      /* FIXME: Ignore these failures? */
+      if (error)
+        return AE_OK;
+      break;
+    default:
+      break;
   }
 
-  Status = AcpiGetObjectInfo(dev, &Info);
-
-  if (ACPI_SUCCESS (Status))
-  {
-    
-    printf (" HID: %s, ADR: %llX, Name: %x\n", Info->HardwareId.String, Info->Address, Info->Name);
-  }
-
-  return NULL;
+  return AE_OK;
 }
 
 /*!
@@ -47,6 +85,8 @@ void init_acpi_devices()
 {
   ACPI_STATUS stat;
   acpi_handle_t sysbus_hndl;
+
+  _acpi_device_list = init_list();
 
   stat = AcpiGetHandle(NULL, ACPI_NS_ROOT_PATH, &sysbus_hndl);
 
